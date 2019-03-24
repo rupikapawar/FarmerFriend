d2b061acfa949feddeab3c34836865a1d6689d45
Rupika Pawar <rupikapawar3197@gmail.com>
2019-03-24 15:59:10 +0530

    kjkjijijii

--- app/src/main/java/com/example/rupik/farmerfriend/MainActivity.java
+++ app/src/main/java/com/example/rupik/farmerfriend/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Blah Blah", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "fuck off", Toast.LENGTH_SHORT).show();
 
     }
 }
+
